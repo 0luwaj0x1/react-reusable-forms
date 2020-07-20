5c62ea3eb4e812fa8eeada56be5ee8fc9efb356f
jilesanmi <ilesanmi.josiah@gmail.com>
2020-07-20 15:36:01 +0100

    Updates

---
+++ precache-manifest.60ad532b836aeaf2095743848044f6e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63bc817ff4e3af3d8e9da1b769a6416",
+    "url": "/react-reusable-forms/index.html"
+  },
+  {
+    "revision": "010c094532e3f236cd75",
+    "url": "/react-reusable-forms/static/css/main.241824a9.chunk.css"
+  },
+  {
+    "revision": "2c20db4204f57b2fe7ea",
+    "url": "/react-reusable-forms/static/js/2.1ef84e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-reusable-forms/static/js/2.1ef84e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010c094532e3f236cd75",
+    "url": "/react-reusable-forms/static/js/main.ac6bb2fb.chunk.js"
+  },
+  {
+    "revision": "d5ce5e58b8ac9bd15c1c",
+    "url": "/react-reusable-forms/static/js/runtime-main.36639d72.js"
+  }
+]);
